b1d8b6b1bc3d57fa2bae4511fec52717ea7b65b6
ConfusedKnight <aarushdangol2006@gmail.com>
2026-02-20 01:45:36 +0545

    Coming soon structurized

---
+++ src/app/components/counter/Counter.tsx
@@ -0,0 +1,94 @@
+"use client";
+import { memo, useEffect, useState } from "react";
+import { cabinetBold, cabinetMedium, cabinetRegular } from "@/app/utils/fonts";
+
+const Counter_wrapper = () => {
+  const [isClient, setisClient] = useState(false);
+  useEffect(() => {
+    setisClient(true);
+  }, []);
+  const Counter_component = memo(Counter);
+  return isClient ? <Counter_component /> : null;
+};
+const Counter = () => {
+  const calculateTimeLeft = (): {
+    days: number;
+    hours: number;
+    minutes: number;
+    seconds: number;
+  } => {
+    const countDownDate = new Date("Jun 11, 2026 17:00:00").getTime();
+    const now = new Date().getTime();
+    const timeLeft = countDownDate - now;
+    return {
+      days: Math.floor(timeLeft / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+      minutes: Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60)),
+      seconds: Math.floor((timeLeft % (1000 * 60)) / 1000),
+    };
+  };
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  });
+
+  const addLeadingZero = (number: number): string => {
+    return number < 10 ? `0${number}` : number.toString();
+  };
+
+  return (
+    <div className="text-magnolia">
+      <div className="flex justify-center">
+        <div className="flex flex-col items-center">
+          <p
+            className={`lg:text-4xl text-2xl font-bold lg:tracking-[.25rem] tracking-[2px] ${cabinetBold.className} `}
+          >
+            {addLeadingZero(timeLeft.days)}:
+          </p>
+          <span className={`lg:text-[15px] text-[10px] mr-4 ${cabinetMedium.className}`}>
+            Days
+          </span>
+        </div>
+        <div className="flex flex-col items-center">
+          <p
+            className={`lg:text-4xl text-2xl font-bold tracking-[.25rem] ${cabinetBold.className} `}
+          >
+            {addLeadingZero(timeLeft.hours)}:
+          </p>
+          <span className={` lg:text-[15px] text-[10px] mr-4 ${cabinetMedium.className}`}>
+            Hours
+          </span>
+        </div>
+        <div className="flex flex-col items-center">
+          <p
+            className={`lg:text-4xl text-2xl font-bold tracking-[.25rem] ${cabinetBold.className} `}
+          >
+            {addLeadingZero(timeLeft.minutes)}:
+          </p>
+          <span className={` lg:text-[15px] text-[10px] mr-4 ${cabinetMedium.className}`}>
+            Minutes
+          </span>
+        </div>
+        <div className="flex flex-col items-center">
+          <p
+            className={`lg:text-4xl text-2xl font-bold tracking-[.25rem]  ${cabinetBold.className} `}
+          >
+            {addLeadingZero(timeLeft.seconds)}
+          </p>
+          <span
+            className={`font-medium lg:text-[15px]  text-[10px] ${cabinetMedium.className}`}
+          >
+            Seconds
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Counter_wrapper;
